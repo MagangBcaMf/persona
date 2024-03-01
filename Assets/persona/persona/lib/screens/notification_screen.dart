d59e1ba7da9c4346ccd92280e623a2544b5b10ba
unknown <reynard.satria@gmail.com>
2024-03-01 11:31:58 +0700

    fetch reminder done

--- Assets/persona/persona/lib/screens/notification_screen.dart
+++ Assets/persona/persona/lib/screens/notification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:persona/repository/repository.dart';
+import 'package:persona/screens/eventList.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
@@ -9,151 +10,23 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  Map<DateTime, List<Event>> selectedEvents = {};
+
+  Future<void> initializeSelectedEvents() async {
+    try {
+      Map<DateTime, List<Event>> events = await Repository.fetchEvents();
+      setState(() {
+        selectedEvents = events;
+      });
+    } catch (e) {
+      print('Error initializing selectedEvents: $e');
+    }
+  }
+
   @override
-  Widget Notification() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const SizedBox(height: 20),
-        ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxHeight: double.infinity,
-            maxWidth: 415,
-          ),
-          child: Center(
-            child: Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(10),
-              child: Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  children: [
-                    const Text(
-                      'Rapat',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Jam 10:00',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Lokasi: Meja IT',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Pengingat: 15 menit sebelum',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.red,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        maxHeight: double.infinity,
-                        maxWidth: double.infinity,
-                      ),
-                      child: const Text(
-                        'Catatan: Bahas mengenai perubahan tampilan home',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: 8),
-        ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxHeight: double.infinity,
-            maxWidth: 415,
-          ),
-          child: Center(
-            child: Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(10),
-              child: Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  children: [
-                    const Text(
-                      'Deadline Ticket',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Jam 13:00',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Lokasi: Marketing',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Pengingat: 10 menit sebelum',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.red,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        maxHeight: double.infinity,
-                        maxWidth: double.infinity,
-                      ),
-                      child: const Text(
-                        'Catatan: Ticket PO harus selesai hari ini!',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
+  void initState() {
+    super.initState();
+    initializeSelectedEvents();
   }
 
   @override
@@ -161,7 +34,6 @@ class _NotificationScreenState extends State<NotificationScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("personA"),
-        // centerTitle: true,
         backgroundColor: const Color(0xff7da0ca),
       ),
       backgroundColor: Colors.white,
@@ -169,10 +41,38 @@ class _NotificationScreenState extends State<NotificationScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Notification(),
+            NotificationWidget(selectedEvents: selectedEvents),
           ],
         ),
       ),
+    );
+  }
+}
+
+class NotificationWidget extends StatelessWidget {
+  final Map<DateTime, List<Event>> selectedEvents;
+
+  const NotificationWidget({Key? key, required this.selectedEvents})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final dateNow = DateTime.now().toLocal();
+    final dateNowOnlyDate = DateTime(dateNow.year, dateNow.month, dateNow.day);
+
+    return Column(
+      children: selectedEvents.entries.map((entry) {
+        final date = entry.key;
+        final events = entry.value;
+
+        final dateOnly = DateTime(date.year, date.month, date.day);
+
+        if (dateOnly == dateNowOnlyDate) {
+          return EventListWidget(events: events);
+        } else {
+          return SizedBox();
+        }
+      }).toList(),
     );
   }
 }
